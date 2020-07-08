57af2b1249c5dd4e4f36bf66624b8a8d018fdc40
GCaplan <gabriel.caplan@gmail.com>
2020-07-07 22:34:11 -0400

    pseudo coded js and html files

---
+++ script.js
@@ -0,0 +1,58 @@
+// // USER STORY
+// AS A coding bootcamp student
+// timed quiz on JavaScript fundamentals
+// that stores high scores
+// // add a share with friends
+// maybe a fun graphic that goes high when points go up
+// (may need to set empty values?)
+
+// STARTING DATA Data from the DOM==============================================
+
+var startEl = document.getElementById("start");
+var questionEl = document.getElementById("question");
+var startEl = document.getElementById("start");
+// choices will be an array of choices
+var choicesEl = document.getElementById("choices");
+var timerEl = document.getElementById("timer");
+
+// Helper Functions==========================================================
+
+// User Interactions (or inputs) =============================================
+
+// Updating Display based on user interactions ===============================
+
+// User will see a message with rules of games
+
+// User will click a start button
+
+// Rules will dissapear
+
+// Start button will dissapear
+
+// Main view appears with questions, multiple answers, timer
+
+// timer starts countdown from 50seconds
+// if counter reaches 0
+// then game is over
+
+// user clicks on multiple choice answer
+
+// if user is correct they get a point
+// and user is alerted "correct"
+
+// else (or is this and "when/then promise?")
+// user is alerted "incorrect"
+// THEN time is subtracted from clock
+
+// user will repeat questions and answer
+
+// WHEN all questions are answered or the timer reaches 0
+// THEN the game is over
+
+// WHEN the game is over
+// Prompt to save user initials and score
+
+// Starting Data (initialization)  ========================
+// DOM Elements
+
+// Clear the form==========================
